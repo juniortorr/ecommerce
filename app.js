fe782c47a4214f3164340938b639d2204bf41703
bella <bella@LAPTOP-PQOTO1M5.(none)>
2021-12-07 15:30:22 -0800

    auth

--- app.js
+++ app.js
@@ -1,37 +1,16 @@
 const express = require('express');
+const cookieSession = require('cookie-session');
+const authRouter = require('./routes/admin/auth');
+
 const app = express();
-const usersRepo = require('./repositories/users');
 
 app.use(express.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-  res.send(`
-    <div>
-        <form method="POST">
-            <input name="email" placeholder="email"/>
-            <input name="password" placeholder="password"/>
-            <input name="passwordConfirmation" placeholder="password confirmation"/>
-            <button>Sign Up</button>
-        </form>  
-    </div>
-  `);
-});
-
-app.post('/', async (req, res) => {
-  const { email, password, passwordConfirmation } = req.body;
-
-  const existingUser = await usersRepo.getOneBy({ email });
-
-  if (existingUser) {
-    return res.send('Email in use');
-  }
-
-  if (password !== passwordConfirmation) {
-    return res.send('Passwords must match');
-  }
-
-  res.send('account created!');
-});
+app.use(
+  cookieSession({
+    keys: ['jhg5ry'],
+  })
+);
+app.use(authRouter);
 
 app.listen(3000, () => {
   console.log('Listening Buddy!');
